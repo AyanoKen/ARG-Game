c6cd7ee37b2fc78f92d3a146fc5637c1e8ddc22a
Kireet Gannavarapu <kireet.gannavarapu@gmail.com>
2024-08-22 23:26:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -278,10 +278,18 @@ app.get('/crossword', (req, res) =>{
 app.post('/crossword/complete', async (req, res) => {
     if(req.isAuthenticated()){
         const userId = req.user.googleId; 
-        const currentDate = new Date();
+        const today = new Date();
+        const yyyy = today.getFullYear();
+        let mm = today.getMonth() + 1; // Months start at 0!
+        let dd = today.getDate();
+
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
+
+        const formattedToday = dd + '-' + mm + '-' + yyyy;
         try {
             const updates = {
-                'levelCompletionDates.3': currentDate
+                'levelCompletionDates.3': formattedToday
             }
 
             const result = await User.findOneAndUpdate(
